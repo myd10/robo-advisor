9d363238ea68deac58443e13734eb4fff512e013
myd10 <myd10@georgetown.edu>
2020-02-24 12:08:47 -0500

    Capitalize CSV stock symbol

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -125,7 +125,7 @@ while run:
     print("-------------------------")
 
     #writing data to CSV
-    csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", f"{symbol}_prices.csv") # a relative filepath
+    csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", f"{symbol.upper()}_prices.csv") # a relative filepath
     csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
     with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
         writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
